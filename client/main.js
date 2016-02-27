8ad6a6e8c69eeae4677f7e78912a2590ef319f67
Adel Qalieh <aqalieh95@gmail.com>
2016-02-27 15:15:18 -0500

    Add Google Maps

--- client/main.js
+++ client/main.js
@@ -1,3 +1,18 @@
+Meteor.startup(function () {
+  GoogleMaps.load();
+});
+
+Template.body.helpers({
+  studyMapOptions: function () {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(39.952048, -75.193718),
+        zoom: 16
+      };
+    }
+  }
+});
+
 Template.roomlist.helpers({
   rooms: function () {
     return Rooms.find({});
